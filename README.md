83b7aff372802b06ec061f06b2959aa86051e88b
Marshall Shen <mshen@groupon.com>
2013-08-13 21:17:02 -0500

    step 3 WIP

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-emma
+Emma
 ====
 
 A small web app written in Ember.js
+
+Progress
+=====
+Following [Ember101](http://ember101.com/) step by step
+
+1. Intro to Ember
+2. Your First Route
+3. Displaying a List of Data (WIP)
+4. Master Detail
+5. Two-way Data Binding
+6. Adding items to a list
+7. Nested and Index Routes
+8. Ember Data Reading
+
--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
-App = Ember.Application.create();
+var App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+App.Router.map(function(){
+  // routes go here
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+  model: function(){
+    return users;
   }
 });
+
+
+var users = [
+  {
+    id: 1,
+    first: 'Ryan',
+    last: 'Florence',
+    avatar: 'https://si0.twimg.com/profile_images/3123276865/5c069e64eb7f8e971d36a4540ed7cab2.jpeg'
+  },
+  {
+    id: 2,
+    first: 'Tom',
+    last: 'Dale',
+    avatar: 'https://si0.twimg.com/profile_images/1317834118/avatar.png'
+  },
+  {
+    id: 3,
+    first: 'Yehuda',
+    last: 'Katz',
+    avatar: 'https://si0.twimg.com/profile_images/3250074047/46d910af94e25187832cb4a3bc84b2b5.jpeg'
+  }
+];
